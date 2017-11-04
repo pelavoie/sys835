1415e4cf5c0998654b3912134484db1d9d1a39de
EricLacerte <eric.lacerte1@gmail.com>
2017-11-04 16:10:42 -0400

    Scaling analysis graphs

--- matlab/analysis_data.m
+++ matlab/analysis_data.m
@@ -1,26 +1,33 @@
-function v_suppression = analysis_data( v_data, sampling_freq, play=false)
+function v_suppression = analysis_data( v_data, sampling_freq, play=false, graph_name='')
 % Plot 2 graphs, time domain and frequecny domain
 % Play the waveforms
 
 subplot(2,1,1);
-sec= (1:length(v_data))*(1/sampling_freq);
-plot(sec,v_data);
-xlim([0 sec(length(sec))]);
-ylim([min(v_data) max(v_data)]);
+time_s= (1:length(v_data))*(1/sampling_freq);
+amp = (v_data);
+plot(time_s,amp);
+xlim([0 time_s(length(time_s))]);
+%ylim([min(amp) max(amp)]);
 xlabel('time(sec)');
-ylabel('signal Amplitude');
+ylabel('signal Amplitude (V)');
 
 subplot(2,1,2);
 v_fft=fft(v_data);
 N=length(v_data);
 freq = (1:length(v_fft)) * (sampling_freq/N);
-plot(freq, abs(v_fft));
+amp = convert_to_db(abs(v_fft)/(0.5*N));
+plot(freq, amp);
 xlim([0 sampling_freq/2]);
-%ylim([min(v_data) max(v_data)]);
+ylim([-90 max(amp)]);
 xlabel('Frequency(Hz)');
-ylabel('signal Amplitude');
+ylabel('signal Amplitude (dB)');
+
+if (!strcmp(graph_name,''))
+  saveas (1, graph_name);
+endif
 
 if (play)
   sound(v_data,sampling_freq);
 endif
+
 endfunction
--- matlab/test_channel_filters.m
+++ matlab/test_channel_filters.m
@@ -1,5 +1,5 @@
 % test of channel filters
-% Based on McAulay and Malpass Speech Enhancement p.141
+clf();
 freq_sample=8000;
 v_fc=[240 360 480 600 720 840 975 1125 1275 1425 1575 1750 1950 2150 2350 2600 2900 3200 3535];
 v_bw=[120 120 120 120 120 120 150 150 150 150 150 200 200 200 300 300 300 300 370];
