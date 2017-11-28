44ef364a7f0172d553d526d191534fe83d4ad933
EricLacerte <eric.lacerte1@gmail.com>
2017-11-27 22:55:17 -0500

    Add functions description

--- projet/CSS/Projet_SYS835/include/utils.h
+++ projet/CSS/Projet_SYS835/include/utils.h
@@ -10,6 +10,43 @@
 
 
 /************************************* Prototypes ***********************************************/
-float GetCalculatedSuppressionValue(unsigned int ulEpsilon, float lMeasuredParms);
-float GetSmoothedSuppressionValue(float lCurrentGain, float lPreviousGain);
-void RecombineSignal( float* lResultData, float* lChannelData, unsigned int ulNbData, unsigned int ulChannelNumber);
+
+/* GetCalculatedSuppressionValue
+ *
+ * 		arguments:
+ * 			- ulEpsilon: Epsilon
+ * 			- lMeasuredParms : Measured parameters.
+ * 		Description:
+ * 			it choose the proper Suppression table (according to ulEpsilon)
+ * 			and determine the value of Suppression with lMeasuredParms
+ * 		returns:
+ * 			- (float) Pre-calculated value of suppression Gain.
+ */
+float GetCalculatedSuppressionValue(const unsigned int ulEpsilon, const float lMeasuredParms);
+
+/* GetSmoothedSuppressionValue
+ *
+ * 		arguments:
+ * 			- lCurrentGain: The current calculated Gain
+ * 			- lPreviousGain : The last applied Gain.
+ * 		Description:
+ * 			it "smooth" the current calculated gain to minimize brutal variations.
+ * 		returns:
+ * 			- (float) Smoothed value of suppression to apply.
+ */
+float GetSmoothedSuppressionValue(const float lCurrentGain, const float lPreviousGain);
+
+/* GetSmoothedSuppressionValue
+ *
+ * 		arguments:
+ * 			- lResultData: Pointer of the Result Array of float data
+ * 			- lChannelData : Pointer of the Channel Array of float data
+ * 			- ulNbData : Number of Data in the Arrays
+ * 			- ulChannelNumber : the number of the channel
+ * 		Description:
+ * 			it add or substract (depending of the channel number) the lChannelData arrays to
+ * 			the lResultData Arrays. The size of lResultData must be the same as lChannelData. *
+ * 		returns:
+ * 			- void, the lResultData arrays is "modified" to the new result.
+ */
+void RecombineSignal(const float* lResultData, const float* lChannelData, const unsigned int ulNbData, const unsigned int ulChannelNumber);
